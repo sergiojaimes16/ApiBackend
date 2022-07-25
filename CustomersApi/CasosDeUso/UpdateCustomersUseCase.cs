82ec82a312f0ae1f069bc4b8b24c41f0cd782dde
SERGIO ANDRÉS JAIMES PIMIENTO <sjaimes@cic.uis.edu.co>
2022-07-25 16:58:27 -0500

    Api terminada

---
+++ CustomersApi/CasosDeUso/UpdateCustomersUseCase.cs
@@ -0,0 +1,39 @@
+﻿using CustomersApi.Dtos;
+using CustomersApi.Repositories;
+
+namespace CustomersApi.CasosDeUso
+{
+  public interface IUpdateCustomerUseCase
+  {
+    Task<CustomerDto?> Execute(CustomerDto customer);
+  }
+
+  public class UpdateCustomersUseCase : IUpdateCustomerUseCase
+  {
+  
+
+    private readonly CustomerDatabaseContext _customerDatabaseContext;
+
+    public UpdateCustomersUseCase(CustomerDatabaseContext customerDatabaseContext)
+    {
+      _customerDatabaseContext = customerDatabaseContext;
+    }
+
+    public async Task<CustomerDto?> Execute(CustomerDto customer)
+    {
+      var entity = await _customerDatabaseContext.Get(customer.Id);
+
+      if (entity == null) return null;
+
+      entity.FirstName = customer.FirstName;
+      entity.LastName = customer.LastName;
+      entity.Email = customer.Email;
+      entity.Phone = customer.Phone;
+      entity.Address = customer.Address;
+
+      await _customerDatabaseContext.Actualizar(entity);
+      return entity.ToDto();
+
+    }
+  }
+}
